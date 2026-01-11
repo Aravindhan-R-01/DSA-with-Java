6dbc988d436cc950a5edae5dfe58942d5fe7a9b2
Aravindhan R <aravindhan029@gmail.com>
2026-01-11 18:17:18 +0530

    Rename class to Day109_Top_K_FrequentElements

--- Day109_Top_K_FrequentElements.java
+++ Day109_Top_K_FrequentElements.java
@@ -1,4 +1,15 @@
-class Top_K_FrequentElements {
+// Top K Frequent Elements:
+// Given an integer array nums and an integer k, return the k most frequent elements within the array.
+// The test cases are generated such that the answer is always unique.
+// You may return the output in any order.
+// Example 1:
+// Input: nums = [1,2,2,3,3,3], k = 2
+// Output: [2,3]
+// Example 2:
+// Input: nums = [7,7], k = 1
+// Output: [7]
+
+class Day109_Top_K_FrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i : nums){
