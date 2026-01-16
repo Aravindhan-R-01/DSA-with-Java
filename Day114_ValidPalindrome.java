72a2e4ebac03254f94ab5a382f7a95a99ab24543
Aravindhan R <aravindhan029@gmail.com>
2026-01-16 23:40:40 +0530

    Add palindrome checking method in Day114_ValidPalindrome
    Implement a method to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

---
+++ Day114_ValidPalindrome.java
@@ -0,0 +1,28 @@
+// Valid Palindrome:
+// Hints
+// Given a string s, return true if it is a palindrome, otherwise return false.
+// A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+// Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+// Example 1:
+// Input: s = "Was it a car or a cat I saw?"
+// Output: true
+// Explanation: After considering only alphanumerical characters we have "wasitacaroracatisaw", which is a palindrome.
+// Example 2:
+// Input: s = "tab a cat"
+// Output: false
+// Explanation: "tabacat" is not a palindrome.
+// Constraints:
+// 1 <= s.length <= 1000
+// s is made up of only printable ASCII characters.
+
+class Day114_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char character : s.toCharArray()){
+            if(Character.isLetterOrDigit(character)){
+                sb.append(Character.toLowerCase(character));
+            }
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
