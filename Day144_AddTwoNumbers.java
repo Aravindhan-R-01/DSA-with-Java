91651fe73ae61e91d769aa270c5e5143fb33325d
Aravindhan R <aravindhan029@gmail.com>
2026-02-15 06:36:54 +0530

    Add method to sum two numbers represented by linked lists
    Implement addTwoNumbers method to sum two linked lists.

---
+++ Day144_AddTwoNumbers.java
@@ -0,0 +1,47 @@
+// Add Two Numbers:
+// You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
+// The digits are stored in reverse order, e.g. the number 321 is represented as 1 -> 2 -> 3 -> in the linked list.
+// Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Return the sum of the two numbers as a linked list.
+// Example 1:
+// Input: l1 = [1,2,3], l2 = [4,5,6]
+// Output: [5,7,9]
+// Explanation: 321 + 654 = 975.
+// Example 2:
+// Input: l1 = [9], l2 = [9]
+// Output: [8,1]
+// Constraints:
+// 1 <= l1.length, l2.length <= 100.
+// 0 <= Node.val <= 9
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Day144_AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+            cur.next = new ListNode(val);
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return dummy.next;
+    }
+}
