6959f44661b3744bde61b1099a846e8628f4b0a1
Aravindhan R <aravindhan029@gmail.com>
2026-02-25 12:43:40 +0530

    Add subtree check for binary trees
    Implement a solution to check if one binary tree is a subtree of another.

---
+++ Day154_SubtreeOfAnotherTree.java
@@ -0,0 +1,45 @@
+// Subtree of Another Tree:
+// Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+// A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+// Example 1:
+// Input: root = [1,2,3,4,5], subRoot = [2,4,5]
+// Output: true
+// Example 2:
+// Input: root = [1,2,3,4,5,null,null,6], subRoot = [2,4,5]
+// Output: false
+// Constraints:
+// 1 <= The number of nodes in both trees <= 100.
+// -100 <= root.val, subRoot.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        if (root == null) return false;
+        if (sameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public boolean sameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;
+        if (root != null && subRoot != null && root.val == subRoot.val) {
+            return sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right);
+        }
+        return false;
+    }
+}
