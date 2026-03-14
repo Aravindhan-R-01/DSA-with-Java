fe5c4aee490685a2136b5870213e89746048618c
Aravindhan R <aravindhan029@gmail.com>
2026-03-14 21:01:47 +0530

    Add subsets method to generate all possible subsets
    Implement a method to generate all subsets of a unique integer array.

---
+++ Day171_Subsets.java
@@ -0,0 +1,30 @@
+// Subsets:
+// Given an array nums of unique integers, return all possible subsets of nums.
+// The solution set must not contain duplicate subsets. You may return the solution in any order.
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+// Input: nums = [7]
+// Output: [[],[7]]
+// Constraints:
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+
+
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(res.get(i));
+                subset.add(num);
+                res.add(subset);
+            }
+        }
+        return res;
+    }
+}
